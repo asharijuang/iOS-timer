95209df057e3bde8b646cb7638588ef1fca7abe6
asharijuang <asharijuang@me.com>
2015-08-14 10:57:13 +0700

    complate logic timer

--- iOS StopWatch/ViewController.swift
+++ iOS StopWatch/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var timeLabel: UILabel!
+    var time = 0
+    var timer = NSTimer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,13 +23,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func runner(){
+        time++
+        timeLabel.text = String(time)
+    }
 
     @IBAction func startTimer(sender: AnyObject) {
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target:self, selector: Selector("runner"), userInfo: nil, repeats: true)
     }
 
     @IBAction func pauseTimer(sender: AnyObject) {
+        timer.invalidate()
     }
     @IBAction func resetTimer(sender: AnyObject) {
+        timer.invalidate()
+        timeLabel.text = "00"
+        time = 0
     }
 }
 
